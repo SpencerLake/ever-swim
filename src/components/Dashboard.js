7fb453e0abfd191bb51866674dfb6556890794af
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-18 13:11:31 -0500

    creating basic layout

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+export default function Dashboard() {
+    return(
+        <div className='dash'>
+            <div className='selection'>
+
+            </div>
+            <div className='featured'>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
