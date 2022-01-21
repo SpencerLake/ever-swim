9472039c07187022902a847268c831b212735718
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-21 12:03:26 -0500

    trying to add in responsive nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,26 @@ import Dashboard from './Dashboard'
 
 function App() {
 
+  const hamburger = document.querySelector(".hamburger")
+  const navMenu = document.querySelector(".nav-menu")
+
   useEffect(() => {
     window.addEventListener('scroll', isSticky)
     return () => {
       window.removeEventListener('scroll', isSticky)
     }
   })
+  useEffect(() => {
+    hamburger.addEventListener("click", mobileMenu)
+    return () => {
+      hamburger.removeEventListener('scroll', mobileMenu)
+    }
+  })
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
 
   const isSticky = (e) => {
     const header = document.querySelector('.header')
@@ -18,18 +32,35 @@ function App() {
   }
 
 
+  
+
+
   return (
     <div className="App">
       <div className='header'>
-        <div className='header-content-left'>
-          <div className='icon'></div>
-          <a className='nav-item'>Tops</a>
-          <a className='nav-item'>Bottoms</a>
-          <a className='nav-item'>One Pieces</a>
-        </div>
-        <div className='header-content-right'>
-          <a className='nav-item'>Login</a>
-        </div>
+        <nav className='nav-menu'>
+          <ul className='header-content-left'>
+            <div className='nav-logo'></div>
+            <li className='nav-item'>
+              {/* Change all p tags into a tags when routes set up */}
+              <p href='#' className='nav-link'>Tops</p>
+            </li>
+            <li className='nav-item'>
+              <p href='#' className='nav-link'>Bottoms</p>
+            </li>
+            <li className='nav-item'>
+              <p href='#' className='nav-link'>One Piece</p>
+            </li>
+          </ul>
+          <div className='hamburger'>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+          </div>
+          <div className='header-content-right'>
+            <a className='nav-item'>Login</a>
+          </div>
+        </nav>
       </div>
       <div className='content'>
         <Dashboard />
